86b51199beb192c2e567eccdc39421f6e7434358
chris-topping-utils <85423266+chris-topping-utils@users.noreply.github.com>
2021-06-07 12:15:47 +1000

    Create Get-Dinopass.ps1

---
+++ Get-Dinopass.ps1
@@ -0,0 +1,11 @@
+﻿# Get-Dinopass
+# Chris Topping 29/4/20
+# Pulls a strong pw from Dinopass > 12 characters
+
+$password = 0
+while($password.length -lt 12)
+{
+    $password = Invoke-RestMethod -uri https://www.dinopass.com/password/strong
+}
+
+Write-Output "Your password is" $password
